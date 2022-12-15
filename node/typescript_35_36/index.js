b56b24ed24f03ca93fde940b95176335ac617232
mkahvedzic <merimakahvedzic91@gmail.com>
2022-12-15 21:28:21 +0100

    added typescript

---
+++ node/typescript_35_36/index.js
@@ -0,0 +1,9 @@
+function saberi(a, b) {
+    return a + b;
+}
+
+var brojA  = 4;
+var brojB  = 5;
+var rezultat = saberi(brojA, brojB);
+
+console.log(rezultat);
---
+++ node/typescript_35_36/index.ts
@@ -0,0 +1,10 @@
+function saberi(a: number, b: number): number {
+    return a + b;
+}
+
+let brojA: number = 4;
+let brojB: number = 5;
+let rezultat = saberi(brojA, brojB);
+
+
+console.log(rezultat);
