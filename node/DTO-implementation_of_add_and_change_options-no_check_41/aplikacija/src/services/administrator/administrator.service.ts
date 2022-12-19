df33f5785ac6c7ee1e5c333b8ec6c20f5ef7bc52
mkahvedzic <merimakahvedzic91@gmail.com>
2022-12-19 15:40:25 +0100

    DTO-implementation of add and change options-no check

---
+++ node/DTO-implementation_of_add_and_change_options-no_check_41/aplikacija/src/services/administrator/administrator.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Administrator } from 'entities/administrator.entity';
+import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
+import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { Repository } from 'typeorm';
+
+
+@Injectable()
+export class AdministratorService {
+    constructor(
+        @InjectRepository(Administrator)
+        private readonly  administrator: Repository<Administrator> 
+    )  { }
+
+   getAll(): Promise<Administrator[]> {
+     return this.administrator.find();
+
+   }
+
+  getById(id: number): Promise<Administrator> {
+          return this.administrator.findOne(id);
+  } 
+  
+   // DTO -> Model
+    // usename -> username
+    // password -[~]-> passwordHash ! Stvar izbora! SHA512
+
+
+   add(data: AddAdministratorDto) {
+        const crypto = require('crypto');
+        
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        let newAdmin: Administrator = new Administrator();
+        newAdmin.username = data.username;
+        newAdmin.passwordHash = passwordHashString;
+
+       return this.administrator.save(newAdmin);
+
+   }
+
+    async editById(id, data: EditAdministratorDto): Promise<Administrator> { 
+      let admin: Administrator = await this.administrator.findOne(id);
+
+      const crypto = require('crypto');
+        
+      const passwordHash = crypto.createHash('sha512');
+      passwordHash.update(data.password);
+
+      const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+      admin.passwordHash = passwordHashString;
+
+      return this.administrator.save(admin);
+   }
+
+ }
+
