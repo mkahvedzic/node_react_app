1f230cc617c92a253bf622346dfe1619903fcfac
mkahvedzic <merimakahvedzic91@gmail.com>
2022-12-21 21:21:04 +0100

    method testing postman web service

---
+++ node/method_testing_postman_web_service_42/aplikacija/src/controllers/api/administrator.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { Administrator } from "entities/Administrator";
+import { ApiResponse } from "misc/api.response.class";
+import { AddAdministratorDto } from "src/dtos/administrator/add.administrator.dto";
+import { AdministratorService } from "src/services/administrator/administrator.service";
+import { Admin } from "typeorm";
+
+@Controller('api/administrator')
+export class AdministratorController {
+   constructor(
+    private administratorService: AdministratorService
+   ) {}
+
+  
+   @Get() 
+   getAll(): Promise<Administrator[]> {
+     return this.administratorService.getAll();
+   }
+
+   
+  @Get(':id') 
+  getById(@Param('id') administratorId: any): Promise<Administrator> {
+    
+    return this.administratorService.getById(administratorId);
+
+  }
+
+  @Put()
+  add( @Body() data: AddAdministratorDto): Promise<Administrator> | ApiResponse {
+        return this.administratorService.add(data);
+  } 
+
+  @Post(':id')
+  edit(@Param('id') id: number, @Body() data: AddAdministratorDto): Promise<Administrator> {
+         return this.administratorService.editById(id, data);
+  }
+
+
+}
---
+++ node/method_testing_postman_web_service_42/aplikacija/src/services/administrator/administrator.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ApiResponse } from 'misc/api.response.class';
+import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
+import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { Repository } from 'typeorm';
+import { Administrator } from '../../../entities/administrator.entity';
+
+
+@Injectable()
+export class AdministratorService {
+    constructor(
+        @InjectRepository(Administrator)
+        private readonly  administrator: Repository<Administrator> 
+    )  { }
+
+   getAll(): Promise<Administrator[]> {
+     return this.administrator.find();
+
+   }
+
+  getById(id: any): Promise<Administrator> {
+          return this.administrator.findOneBy(id);
+      }
+   
+  add(data: AddAdministratorDto): Promise<Administrator> | ApiResponse {
+      const crypto = require('crypto');
+
+      const passwordHash = crypto.createHash('sha512');
+      passwordHash.update(data.password);
+
+      const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+      let newAdmin: Administrator = new Administrator();
+      newAdmin.username = data.username;
+      newAdmin.passwordHash = passwordHashString;
+
+      return new Promise((resolve) => {
+        this.administrator.save(newAdmin)
+        .then(data => resolve(data))
+        .catch(error => {
+        const response: any = new ApiResponse("error", -1001);
+            resolve(response);
+        });
+        
+      }); 
+  }
+
+      async editById(id: any, data: EditAdministratorDto): Promise<Administrator> {
+        let admin: Administrator = await this.administrator.findOneBy(id);
+
+        const crypto = require('crypto');
+
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+          admin.passwordHash = passwordHashString;
+
+          return this.administrator.save(admin);
+      }
+
+   }
+     
+
+
+  
+
